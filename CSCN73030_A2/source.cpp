3b3ab4fca15c140554e2e277820c0b7556d7c34f
Benjamin Smith <90717941+benjaminpsmith@users.noreply.github.com>
2024-09-19 18:42:45 -0400

    Step 3 completed.

--- CSCN73030_A2/source.cpp
+++ CSCN73030_A2/source.cpp
@@ -1,3 +1,38 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+// Custom structure storing student data (first and last name)
+struct STUDENT_DATA {
+	std::string firstName;
+	std::string lastName;
+};
+
 int main(void) {
+
+	// Define our information needed for processing
+	const std::string studentDataFile = "studentData.txt";	// File to read the student data in from
+	std::vector<STUDENT_DATA> studentData;					// Declare vector with our custom structure
+
+	// Perform file IO
+	std::ifstream filein(studentDataFile);					// Open file for reading
+
+	if (!filein.is_open()) {								// Check if file is open before modifying (exit if not open)
+		std::cerr << "File is not open. Exiting safely\n";
+		return 0;
+	}
+
+	std::string line;
+	while (std::getline(filein, line)) {					// Read in each line of the file until EOF is found
+		std::istringstream iss(line);						// Create an input string stream that will store each line as an object
+		STUDENT_DATA temp_student;							// Create a temporary student object to store the data	
+		iss >> temp_student.firstName >> temp_student.lastName;		// Read in the first and last name from the line
+		studentData.push_back(temp_student);				// Add the student to the vector
+	}
+
+	filein.close();											// Close the file
+
 	return 1;
 }
