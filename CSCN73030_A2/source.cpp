d08f0464e590b9e7c343c4d2f66bd1c334ce7fc2
Benjamin Smith <90717941+benjaminpsmith@users.noreply.github.com>
2024-09-20 08:51:07 -0400

    Updated source.cpp to contain PRE_RELEASE compiler directives.

--- CSCN73030_A2/source.cpp
+++ CSCN73030_A2/source.cpp
@@ -4,43 +4,75 @@
 #include <iostream>
 #include <sstream>
 
+#define PRE_RELEASE
+
 // Custom structure storing student data (first and last name)
 struct STUDENT_DATA {
+	std::string lastName;
 	std::string firstName;
-	std::string lastName; 
-
-	void displayStudentInfo() {
+	std::string email;
+	
+	void displayStudentInfo() {	// This was not updated to display email as the requirements did not ask for it
 		std::cout << "First Name: " << firstName << ", Last Name: " << lastName << std::endl;
 	}
 };
 
 int main(void) {
 
+	// Print out a message to state the current source code type (standard or pre-release)
+#ifdef PRE_RELEASE
+	std::cout << "Pre-Release Source Code\n";
+#else
+	std::cout << "Standard Source Code\n";
+#endif
+
+
 	// Define our information needed for processing
-	const std::string studentDataFile = "studentData.txt";	// File to read the student data in from
+#ifdef PRE_RELEASE
+	const std::string studentDataFile = "studentData_Emails.txt";	// File to read the student data in from
+#else
+	const std::string studentDataFile = "studentData.txt"
+#endif
+
+
 	std::vector<STUDENT_DATA> studentData;					// Declare vector with our custom structure
 
 	// Perform file IO
 	std::ifstream filein(studentDataFile);					// Open file for reading
+
 
 	if (!filein.is_open()) {								// Check if file is open before modifying (exit if not open)
 		std::cerr << "File is not open. Exiting safely\n";
 		return 0;
 	}
 
+
 	std::string line;
 	while (std::getline(filein, line)) {					// Read in each line of the file until EOF is found
 		std::istringstream iss(line);						// Create an input string stream that will store each line as an object
-		STUDENT_DATA temp_student;							// Create a temporary student object to store the data	
-		iss >> temp_student.lastName >> temp_student.firstName;		// Read in the first and last name from the line
+		STUDENT_DATA temp_student;							// Create a temporary student object to store the data
+		
+
+	#ifdef PRE_RELEASE										// Parse through the iss object to get the first name, last name, and email
+		std::string first, last, email;						// Uses comma as a delimiter
+		std::getline(iss, last, ',');
+		std::getline(iss, first, ',');
+		std::getline(iss, email, ',');
+		temp_student.lastName = last;
+		temp_student.firstName = first;
+		temp_student.email = email;
+	#else													// Read in each line, using whitespace as a delimeter
+		iss >> temp_student.lastName >> temp_student.firstName;		
+	#endif
 		studentData.push_back(temp_student);				// Add the student to the vector
 	}	
 
 	filein.close();											// Close the file
 
-	#ifdef _DEBUG
+
+#ifdef _DEBUG
 	for (int i = 0; i < studentData.size(); i++) { studentData[i].displayStudentInfo(); } // Display student data when in DEBUG mode
-	#endif	
+#endif	
 
 	return 1;
 }
